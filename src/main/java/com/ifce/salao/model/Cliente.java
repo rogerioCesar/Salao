1f9110137232e8aed8b937bd76e57610e6d6a5ee
rogerioCesar <rogerionqn1@hotmail.com>
2022-05-05 18:55:29 -0300

    primeiro Commit

---
+++ src/main/java/com/ifce/salao/model/Cliente.java
@@ -0,0 +1,42 @@
+package com.ifce.salao.model;
+
+public class Cliente {
+
+	private String nome;
+	private String cpf;
+	private int idade;
+	private String endereco;
+	private String telefone;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	
+	
+}
